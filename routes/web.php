3625de61de7b1091b721aa6a109956c957c3f716
Lorena.L.P <107314914+Lorena-Pinheiro@users.noreply.github.com>
2024-08-15 12:07:32 -0300

    .

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('paginaInicio');
+});
+
+Route::get('/layoutPaginas', function () {
+    return view('layoutPaginas');
 });
