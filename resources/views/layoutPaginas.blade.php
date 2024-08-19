6f6e50f3ec08d63b1ce576965bdc75d3b6032831
Lorena.L.P <107314914+Lorena-Pinheiro@users.noreply.github.com>
2024-08-19 11:07:19 -0400

    .

--- resources/views/layoutPaginas.blade.php
+++ resources/views/layoutPaginas.blade.php
@@ -3,11 +3,39 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Layout</title>
     <link rel="stylesheet" href="../css/layoutPaginas.css">
 </head>
 <body>
-    hello
+    <nav class="navbar">
+        <a class="navbar-logo" href="paginaInicio">
+            <img src="../images/Logo_Navbar.png" width="48" height="48" class="d-inline-block align-top" alt="">
+        </a>
 
+        <ul class="nav-links">.
+            <a class="link-navbar" href="blog">Blog</a>
+            <a class="link-navbar" href="quemSomos">Quem Somos</a>
+            <a class="link-navbar" href="comoAjudar">Como Ajudar</a>
+            <a class="link-navbar" href="eventos">Eventos</a>
+            <a class="link-navbar" href="adoção">Adoção</a>
+        </ul>
+        
+        <a class="navbar-logo" href="#">
+            <img src="../images/Icon_Login.png" width="48" height="48" alt="">
+        </a>
+    </nav>
+
+    <footer>
+        <a class="footer-logo" href="paginaInicio">
+            <img src="../images/Logo_Footer.png" width="201" height="111" alt="">
+        </a>
+        <ul class="footer-links">
+            <li><a href="blog">Blog</a></li>
+            <li><a href="quemSomos">Quem Somos</a></li>
+            <li><a href="comoAjudar">Como Ajudar</a></li>
+            <li><a href="eventos">Eventos</a></li>
+            <li><a href="adoção">Adoção</a></li>
+        </ul>
+    </footer>
 </body>
 </html>
