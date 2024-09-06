4bfc576d6d36322eeddc203c9d39d1e71847974a
KauaOSantos <kauaoliveira1506gaf@gmail.com>
2024-09-06 11:55:23 -0300

    #89 - Adicionei os ícones de valores para doação, alterei o fundo do texto Ponto de Arrecadação para a cor verde escuro e a cor da letra para branco.

--- resources/views/comoAjudar.blade.php
+++ resources/views/comoAjudar.blade.php
@@ -1,17 +1,85 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Sos Patinhas - Como Ajudar</title>
     <link rel="stylesheet" href="../css/comoAjudar.css">
 </head>
 <body>
     @include('templates.navbarUser')
 
-    <section class="corpo container" >
-        <p>aaaa</p>
+    <section class="hero">
+    <div class="corpo container" >
+        <!-- começei a partir daqui -->
+        <img src="../images/valinhos07 1.png" alt="Imagem da ONG Sos Patinhas" style="width: 453px; height: 283px;">
+        <div class="hero-text">
+        <h2>Ponto de Arrecadação</h2>
+        <p>"Seu gesto de doação é o elo que une corações humanos e patas amigas, criando um mundo mais compassivo para todos."</p>
+        <p>R. do Parque, 75 - Centro, Campo Grande - MS</p>
+        </div>
+      </div>
     </section>
+
+    <section class="valores">
+      <div class="container">
+        <h2>Valores para Doação</h2>
+        <div class="cards">
+          <div class="card">
+            <img src="../images/iconeTalher.png" alt="Ícone de talher">
+            <h3>Kit Ração</h3>
+            <p>R$ 50</p>
+          </div>
+          <div class="card">
+            <img src="../images/iconeLimpeza.png" alt="Ícone de Limpeza">
+            <h3>Kit Limpeza</h3>
+            <p>R$ 25</p>
+          </div>
+          <div class="card">
+            <img src="../images/iconeConforto.png" alt="Ícone de Conforto">
+            <h3>Kit Conforto</h3>
+            <p>R$ 50</p>
+          </div>
+          <div class="card">
+            <img src="../images/iconeMedico.png" alt="Ícone de Médico">
+            <h3>Kit Médico</h3>
+            <p>R$ 100</p>
+          </div>
+          <div class="card">
+            <img src="../images/iconeAtendimento.png" alt="Ícone de Atendimento">
+            <h3>Kit Atendimento</h3>
+            <p>R$ 60</p>
+          </div>
+          <div class="card">
+            <img src="../images/iconeVacina.png" alt="Ícone de Vacina">
+            <h3>Kit Vacinação</h3>
+            <p>R$ 40</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="pix">
+      <div class="container">
+        <h2>Pix QR Code ou CNPJ</h2>
+        <div class="qrcode">
+          <img src="qrcode.png" alt="QR Code para Doação">
+          <p>ChaveCNPJ: XX. XXX. XXX/0001-XX</p>
+        </div>
+        <div class="info">
+          <h3>Sos Patinhas</h3>
+          <p>Conta: 198655-4</p>
+          <p>Agência: 1193</p>
+        </div>
+      </div>
+    </section>
+  </main>
+
+  <footer>
+    <div class="container">
+      <p>&copy; 2023 Sos Patinhas</p>
+    </div>
+  </footer>
 
     @include('templates.footerUser')
 </body>
