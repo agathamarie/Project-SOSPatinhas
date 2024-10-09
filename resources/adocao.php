9d7967398bc9bf923ed578891f794588b7d78750
Lorena-Pinheiro <107314914+Lorena-Pinheiro@users.noreply.github.com>
2024-10-09 08:56:58 -0300

    .

---
+++ resources/adocao.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/adocao.css">
+</head>
+<body>
+    <?php include ('../templates/navbarUser.php') ?>
+    
+
+    <section class="corpo container" >
+
+        <section class="top-texts">
+            <h1>Adote um melhor amigo!</h1>
+            <p>Adotar um animal salva vidas e traz alegria. Muitos cães e gatos esperam por um lar. Ao adotar, você ganha um amigo fiel e combate o abandono. Adote e transforme vidas!</p>
+        </section>
+        
+        <section class="dropdowns">
+            
+        </section class="card-container">
+
+        
+
+        </section>
+
+    </section>
+
+    <?php include ('../templates/footerUser.php') ?>
+</body>
+</html>
---
+++ templates/animalCard.php
@@ -0,0 +1,14 @@
+<div class="flip-card">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="../images/Rochele.png" alt="Rochele" style="width:275px;height:362px;">
+                    </div>
+                    <div class="flip-card-back">
+                        <p>Femea</p>
+                        <p>4 anos</p>
+                        <p>Porte Pequeno</p>
+                        <p>Sem Raça Definida</p>
+                        <a href="informacaoAnimal">Adotar!</a>
+                    </div>
+                </div>
+            </div>
