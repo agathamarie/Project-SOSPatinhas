7c13ee14a302d4cdfa1c4a36a95eae8c2b937415
agathamarie <agatha06arantes@gmail.com>
2024-10-09 09:43:50 -0300

    .

--- resources/paginaInicio.blade.php
+++ resources/paginaInicio.blade.php
@@ -1,18 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="../css/paginaInicio.css">
-</head>
-<body>
-    @include('templates.navbarUser')
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link rel="stylesheet" href="../css/paginaInicio.css">
+    </head>
+    <body>
+        <header>
+            <img src="" alt="">
+        </header>    
 
-    <section class="corpo container" >
-        <p>aaaa</p>
-    </section>
 
-    @include('templates.footerUser')
-</body>
+        <section id="carrosel" >
+            <div id="divcarrosel">
+                <img src="../images/fotoinicio1.png" alt="Foto do Carrosel">
+                <img src="" alt="Foto do Carrosel">
+                <img src="../images/fotoinicio3.png" alt="Foto do Carrosel">
+            </div>
+        </section>
+
+        <?php include('../templates/footerUser.php') ?>
+    </body>
 </html>
